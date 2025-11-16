d5155bbe6c268eb8425ba9214a9a09d29ce7240f
xxxx <xxxx@gmail.com>
2025-11-16 23:40:38 +0800

    Revert "Merge pull request #3 from delus01/fix_int_to_bool_issue"
    This reverts commit 188f7d88e53869dc3e9c5bca1dc14acb0786720e, reversing
    changes made to a0a1637340ea563e5dd780b340c72886bf06326a.

--- lib/src/features/plan/plan_api.dart
+++ lib/src/features/plan/plan_api.dart
@@ -3,7 +3,6 @@ import 'package:flutter_xboard_sdk/src/features/plan/plan_models.dart';
 import 'package:flutter_xboard_sdk/src/common/models/api_response.dart';
 import 'package:flutter_xboard_sdk/src/exceptions/xboard_exceptions.dart';
 
-
 class PlanApi {
   final HttpService _httpService;
 
@@ -14,9 +13,7 @@ class PlanApi {
     try {
       final result = await _httpService.getRequest('/api/v1/user/plan/fetch');
       return ApiResponse.fromJson(result, (json) => (json as List<dynamic>).map((e) => Plan.fromJson(e as Map<String, dynamic>)).toList());
-    } catch (e, stack) {
-      print('Error: $e');
-      print('Stack trace:\n$stack');
+    } catch (e) {
       if (e is XBoardException) rethrow;
       throw ApiException('获取套餐列表时发生错误: $e');
     }
